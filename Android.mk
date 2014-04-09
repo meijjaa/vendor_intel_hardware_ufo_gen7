72455585148b8dc8f078f1d4c67eb6a2d8f6c0e8
Russell Webb <russell.webb@intel.com>
2014-04-09 08:10:49 -0700

    Manual generation of VPG prebuilts (32 bit)
    Built from Tag: abt/ro/vpg-p4/INT_15_33-cs00-ci-15.33-20915
    
    Change-Id: I7f801584fb99f5d2fe5c50974a9f153a654ab936

--- Android.mk
+++ Android.mk
@@ -1,15 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(USE_INTEL_UFO_DRIVER),true)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ufo.prop
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_OWNER := intel
-LOCAL_SRC_FILES := ufo.prop
-include $(BUILD_PREBUILT)
+ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true)
 
 include $(LOCAL_PATH)/$(TARGET_ARCH)/baytrail.mk
 
+endif # BOARD_HAVE_GEN_GFX_SRC 
 endif # USE_INTEL_UFO_DRIVER
