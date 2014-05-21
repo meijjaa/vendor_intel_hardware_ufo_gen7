b74d717a340797090233e49a09001a5255594165
Rafael Barbalho <rafael.barbalho@intel.com>
2014-05-21 18:36:38 +0100

    Add support to filter out gen7 binaries
    With gen8 support coming online the build system needs to be able to
    filter out the different type of binaries. By default the gen7 based
    binaries will still be picked, but these can now be filtered out by
    using the UFO_ENABLE_GEN variable.
    
    Issue: GMIN-971
    Change-Id: I355216c8315383db7d53aec5adad14c8f5a49c62
    Signed-off-by: Rafael Barbalho <rafael.barbalho@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
+# Maintain the default to gen7 until the build targets
+# are sorted out.
+UFO_ENABLE_GEN ?= gen7
+
+ifeq ($(strip $(UFO_ENABLE_GEN)),gen7)
 ifeq ($(USE_INTEL_UFO_DRIVER),true)
 ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true)
 
@@ -7,3 +12,4 @@ include $(LOCAL_PATH)/$(TARGET_ARCH)/baytrail.mk
 
 endif # BOARD_HAVE_GEN_GFX_SRC 
 endif # USE_INTEL_UFO_DRIVER
+endif # UFO_ENABLE_GEN
